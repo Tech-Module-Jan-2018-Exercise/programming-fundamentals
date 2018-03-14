493b5df16996808e61b9d02e05fca110a6f1888d
MossGoblin <hugolabs@gmail.com>
2018-03-14 19:48:13 +0200

    Uploading project files to work with

---
+++ intro-and-basic-syntax/exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+            int numFour = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{numOne:D4} {numTwo:D4} {numThree:D4} {numFour:D4} {numOne:00.00}");
+
+        }
+    }
+}
