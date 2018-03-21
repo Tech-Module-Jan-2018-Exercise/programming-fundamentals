e457aa7c5a10e29f2cd3c7ab72cc7c023c201d68
Martu <martinpismymail@gmail.com>
2018-03-21 19:40:35 +0200

    Exercise03-MilesToKilometers

---
+++ intro-and-basic-syntax/Problem_3/MilesToKilometers/MilesToKilometers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var distanceInMiles = double.Parse(Console.ReadLine());
+
+            double kilometersPerMile = 1.60934;
+
+            var distanceInKilometers = distanceInMiles * kilometersPerMile;
+
+
+
+            Console.WriteLine("{0:F2}", distanceInKilometers);
+        }
+    }
+}
